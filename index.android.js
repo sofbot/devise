30567539d623801f112ab31a45c30fe45c6c37cf
Fariba <massah.2@gmail.com>
2017-04-05 19:19:20 -0700

    Reconcile conflicting file structures

--- index.android.js
+++ index.android.js
@@ -1,92 +1,25 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
-
 
 import React, { Component } from 'react';
 import {
   AppRegistry,
-  StyleSheet,
-  Text,
   View
 } from 'react-native';
+import App from './src/app';
+import configureStore from './src/store/store.js';
 
-import FBSDK from 'react-native-fbsdk';
-const { LoginButton, AccessToken } = FBSDK;
+//bootstrap asyncStorage
 
-export default class Devise extends Component {
-  constructor(props){
-    super(props);
-  }
 
-  // fetchUserInfo(token){
-  //   const infoRequest = new GraphRequest(
-  //     '/me',
-  //     {
-  //       accessToken: token,
-  //       parameters: {
-  //         fields: {
-  //           string: 'id,first_name,last_name,friends,picture,email'
-  //         }
-  //       }
-  //     },
-  //     this._responseInfoCallback
-  //   );
-  //   const one = new GraphRequestManager();
-  //   const two = one.addRequest(infoRequest);
-  //   console.log("post two", two);
-  //   const three = two.start();
-  // }
 
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          devise
-        </Text>
-        <LoginButton
-          readPermissions={["public_profile", "user_friends"]}
-          onLoginFinished={
-            (error, result) => {
-              if (error) {
-                alert("Login failed with error: " + result.error);
-              } else if (result.isCancelled) {
-                alert("Login was cancelled");
-              } else {
-                const token = AccessToken.getCurrentAccessToken().toString();
-                this.fetchUserInfo(token);
-              }
-            }
-          }
-          onLogoutFinished={() => alert("User logged out")}/>
-      </View>
-    );
-  }
-}
+const Devise = () => {
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#2f4f4f',
-  },
-  welcome: {
-    fontSize: 75,
-    textAlign: 'center',
-    margin: 10,
-    fontWeight: "900",
-    color: 'aliceblue'
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+  return (
+    <View>
+        <App />
+    </View>
+  );
+
+};
 
 
 AppRegistry.registerComponent('Devise', () => Devise);
