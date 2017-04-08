db8b242c8232386fda2b9f48bd4b4ad8e76f7cc3
Fariba <massah.2@gmail.com>
2017-04-08 14:59:34 -0700

    Add timeline sorter

--- src/reducers/selectors.js
+++ src/reducers/selectors.js
@@ -2,7 +2,7 @@
 export const dateCompare = (ev1, ev2) => {
   let {startDate, startTime} = ev1;
   let ev1Time = Date.parse(`${startDate} ${startTime}`);
-  let {startDate, startTime} = ev2;
+  let {startDate2, startTime2} = ev2;
   let ev2Time = Date.parse(`${startDate} ${startTime}`);
   let diff = ev1Time - ev2Time;
   return diff;
