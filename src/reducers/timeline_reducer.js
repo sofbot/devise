a836e5ecd931674044a9a44f77836b3692198e3f
Fariba <massah.2@gmail.com>
2017-04-09 02:07:54 -0700

    Complete Timeline View

--- src/reducers/timeline_reducer.js
+++ src/reducers/timeline_reducer.js
@@ -4,10 +4,12 @@ import {
 
 const _defaultState = [
   {
-    title: 'swipe right to add events to your timeline',
-    venue: '',
-    friends: [], // friends also going
-    time: '',
+    title: 'Free Admission Day: Museum of Craft and Design',
+    venue: 'Museum',
+    friends: [{name: "Olivia"}, {name: "Liam"}, {name: "Noah"}], // friends also going
+    time: '13:00:00',
+    description: 'A special free admission day thanks for Sunday Streets. Get inspired by Wendy Maruyama’s magnificent elephant artworks, and scale it down to a wearable size. We’ll be making elephant-inspired shrink plastic pins, charms and pendants that you will be able to wear home, to spread the word about elephant conservation! The museum will be open with FREE admission and activity as a part of the Dogpatch/Bayview Sunday Streets event. The South-bound lanes of 3rd will be closed to traffic, so be sure to bring your bike, skates, or scooter!',
+    imageUrl: 'http://cdn.funcheap.com/wp-content/uploads/2013/10/MOCAD-0261a_small_web-175x130.jpg'
   }
 ];
 
