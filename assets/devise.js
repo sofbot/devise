162aacb5b3d3ff8b46ac72eb17e4a057a0c638bf
Fariba <massah.2@gmail.com>
2017-05-08 19:04:20 -0700

    Update site

--- assets/devise.js
+++ assets/devise.js
@@ -1,6 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const headerBtn = document.getElementById('header-btn');
-  headerBtn.onclick = () => {
-    window.location = './demo.html';
+  const googlePlay = document.getElementById('google-play');
+  googlePlay.onclick = () => {
+    window.alert("Coming soon!");
   };
+  const demoButton = document.getElementById('demo-btn');
+  const demo = document.getElementById('demo');
+  demoButton.onclick = () => window.scrollBy(0, window.innerHeight);
+  // demoButton.onclick = () => demo.scrollIntoView({ behavior: 'smooth' });
 });
