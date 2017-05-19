ae8af6fee9443d8bc035351dd6d2b56163462018
fafafariba <fafafariba@users.noreply.github.com>
2017-05-18 22:18:11 -0700

    Add LoadReducer to RootReducer

--- src/reducers/root_reducer.js
+++ src/reducers/root_reducer.js
@@ -3,11 +3,13 @@ import { combineReducers } from 'redux';
 import UserReducer from './user_reducer.js';
 import EventsReducer from './events_reducer.js';
 import TimelineReducer from './timeline_reducer.js';
+import LoadReducer from './load_reducer.js';
 
 const RootReducer = combineReducers({
   user: UserReducer,
   fetchedEvents: EventsReducer,
-  timelineEvents: TimelineReducer
+  timelineEvents: TimelineReducer,
+  loading: LoadReducer
 });
 
 export default RootReducer;
